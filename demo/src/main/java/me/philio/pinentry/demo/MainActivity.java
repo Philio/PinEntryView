cd6f922dc724417d93d994f8dc8bb2bedd3cb903
Benjamin Deroche <benjamin.deroche@crisalid.com>
2019-08-02 10:20:08 +0200

    Upgrade seriously outdated config

--- demo/src/main/java/me/philio/pinentry/demo/MainActivity.java
+++ demo/src/main/java/me/philio/pinentry/demo/MainActivity.java
@@ -1,20 +1,19 @@
 package me.philio.pinentry.demo;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import me.philio.pinentry.PinEntryView;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends AppCompatActivity {
     private PinEntryView pinEntryView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pinEntryView = (PinEntryView) findViewById(R.id.pin_entry_simple);
+        pinEntryView = findViewById(R.id.pin_entry_simple);
         pinEntryView.setOnPinEnteredListener(new PinEntryView.OnPinEnteredListener() {
             @Override
             public void onPinEntered(String pin) {

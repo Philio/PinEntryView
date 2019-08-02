8a3fbc5d794036ef5386eee3825320517bdea00e
Benjamin Deroche <benjamin.deroche@crisalid.com>
2019-08-02 10:30:16 +0200

    Migrate to AndroidX

--- demo/src/main/java/me/philio/pinentry/demo/MainActivity.java
+++ demo/src/main/java/me/philio/pinentry/demo/MainActivity.java
@@ -1,7 +1,7 @@
 package me.philio.pinentry.demo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 import me.philio.pinentry.PinEntryView;
